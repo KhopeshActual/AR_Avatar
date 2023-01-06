a60ea47c1878bce1317ef7721bfafca714f13e8c
Jon <buckylovesit@gmail.com>
2023-01-06 13:52:54 -0700

    scene changes

--- Assets/ARTESTS/MYScripts/SceneController.cs
+++ Assets/ARTESTS/MYScripts/SceneController.cs
@@ -12,12 +12,12 @@ public class SceneController : MonoBehaviour
 
     public void ShirtScene()
     {
-        SceneManager.LoadScene(SceneManager.GetSceneByName("AR Shirt scene").name); // loads shirt scene
+        SceneManager.LoadScene("AR Shirt Scene", LoadSceneMode.Single); // loads shirt scene
     }
 
     public void AvatarScene()
     {
-        SceneManager.LoadScene(SceneManager.GetSceneByName("AR Avatar Scene").name); // loads avatar scene
+        SceneManager.LoadScene("AR Avatar Scene", LoadSceneMode.Single); // loads avatar scene
     }
 
 }
