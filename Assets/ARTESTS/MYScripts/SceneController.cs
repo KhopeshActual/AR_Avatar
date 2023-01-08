5646adab4cad9663c83a6ba96b6782eb14f85572
Jon <buckylovesit@gmail.com>
2023-01-07 19:22:17 -0700

    UPDATE PREFINAL

--- Assets/ARTESTS/MYScripts/SceneController.cs
+++ Assets/ARTESTS/MYScripts/SceneController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.XR.ARFoundation;
 
 public class SceneController : MonoBehaviour
 {
@@ -12,11 +13,15 @@ public class SceneController : MonoBehaviour
 
     public void ShirtScene()
     {
+        LoaderUtility.Deinitialize();
+        LoaderUtility.Initialize();
         SceneManager.LoadScene("AR Shirt Scene_V2a", LoadSceneMode.Single); // loads shirt scene
     }
 
     public void AvatarScene()
     {
+        LoaderUtility.Deinitialize();
+        LoaderUtility.Initialize();
         SceneManager.LoadScene("AR Avatar Scene", LoadSceneMode.Single); // loads avatar scene
     }
 
